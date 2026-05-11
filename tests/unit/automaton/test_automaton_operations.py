d46a774e83df3a7e264536d02015771ead47114a
YonyUk <yonatanjoseguerraperez@gmail.com>
2026-05-11 11:26:05 -0400

    adds test_automaton_union_operation_10

--- tests/unit/automaton/test_automaton_operations.py
+++ tests/unit/automaton/test_automaton_operations.py
@@ -668,3 +668,31 @@ class TestAutomatonOperations:
 
         assert union_automaton.accept(list(string)) == (one_terminated_dfa.accept(list(string)) or dfa.accept(list(string)))
         assert minimized.accept(list(string)) == union_automaton.accept(list(string))
+    
+    @pytest.mark.parametrize("string",[
+        '',
+        '0',
+        '1',
+        '00',
+        '11',
+        '10',
+        '01',
+        '101',
+        '010',
+        '110',
+        '011',
+        '001',
+        '100',
+        '1010',
+        '10101',
+        '01010',
+        '10100',
+        '101010'
+    ])
+    def test_automaton_union_operation_10(self,string:str,five_multiplo_dfa:DFA,alternate_dfa:DFA):
+
+        union_automaton = Automaton.Union({five_multiplo_dfa,alternate_dfa}).to_deterministic()
+        minimized = union_automaton.minimize()
+
+        assert union_automaton.accept(list(string)) == (five_multiplo_dfa.accept(list(string)) or alternate_dfa.accept(list(string)))
+        assert minimized.accept(list(string)) == union_automaton.accept(list(string))
