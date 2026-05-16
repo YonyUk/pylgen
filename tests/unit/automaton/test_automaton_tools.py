840be01adf2785ed139555e4700509798aac7574
YonyUk <yonatanjoseguerraperez@gmail.com>
2026-05-16 11:27:08 -0400

    adds test_get_words_automaton

--- tests/unit/automaton/test_automaton_tools.py
+++ tests/unit/automaton/test_automaton_tools.py
@@ -1,6 +1,10 @@
+from typing import List
+from string import ascii_letters
+import random
+
 import pytest
 
-from automaton import get_word_automaton
+from automaton import get_word_automaton,get_words_automaton
 
 class TestAutomatonTools:
 
@@ -18,3 +22,21 @@ class TestAutomatonTools:
         assert not aut.accept(list('prove'+string))
         assert not aut.accept(list('not'+string))
         assert not aut.accept(list('prove'+string+'nothing'))
+    
+    @pytest.mark.parametrize("words",[
+        ['hello','world'],
+        ['buscando','a','nemo'],
+        ['welcome','to','the','hell']
+    ])
+    def test_get_words_automaton(self,words:List[str]):
+        aut = get_words_automaton(words).to_deterministic()
+        minimized = aut.minimize()
+
+        for word in words:
+            assert aut.accept(list(word))
+            assert minimized.accept(list(word))
+        
+        for _ in range(20):
+            word = ''.join(random.choices(ascii_letters,k=5))
+            if not word in words:
+                assert not aut.accept(list(word))
