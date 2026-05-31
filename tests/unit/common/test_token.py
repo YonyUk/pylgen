71ecb143cb6795796538528c776cee690f613f7d
YonyUk <yonatanjoseguerraperez@gmail.com>
2026-05-31 14:54:36 -0400

    fix: type error on tests

--- tests/unit/common/test_token.py
+++ tests/unit/common/test_token.py
@@ -6,7 +6,7 @@ class TestToken:
     def test_token_creation(self):
 
         n = Symbol('n',True)
-        t = Token('12',TokenType.GARBAGE,n,1,0)
+        t = Token('12',f'{TokenType.GARBAGE}',n,1,0)
 
         assert t.text == '12'
         assert t.symbol == n
