62deba5ea5928f2fa8435ce72ad08f310486ab42
YonyUk <yonatanjoseguerraperez@gmail.com>
2026-05-31 16:41:20 -0400

    fix error

--- tests/unit/common/test_token.py
+++ tests/unit/common/test_token.py
@@ -1,15 +1,18 @@
 from common.types import Symbol,Token
 from common.enums import TokenType
 
+class TokenTypeEnumForTest(TokenType):
+    GARBAGE = 'GARBAGE'
+
 class TestToken:
 
     def test_token_creation(self):
 
         n = Symbol('n',True)
-        t = Token('12',f'{TokenType.GARBAGE}',n,1,0)
+        t = Token('12',f'{TokenTypeEnumForTest.GARBAGE}',n,1,0)
 
         assert t.text == '12'
         assert t.symbol == n
-        assert t.type == TokenType.GARBAGE
+        assert t.type == TokenTypeEnumForTest.GARBAGE
         assert t.line == 1
         assert t.column == 0
