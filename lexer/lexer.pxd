3cd24e0364cb6dfb9b759ab548ef5bdcb49bb95b
YonyUk <yonatanjoseguerraperez@gmail.com>
2026-06-19 14:47:16 -0400

    adds add_rule method and errors property for lexical errors cacthing

--- lexer/lexer.pxd
+++ lexer/lexer.pxd
@@ -1,4 +1,8 @@
 from .base_lexer cimport BaseLexer
+from analisis.lexical cimport LexicRule
+from analisis.error cimport LexicError
 
 cdef class Lexer(BaseLexer):
-    pass
+    cdef dict[object,set[LexicRule]] _rules
+    cdef set[LexicError] _errors
+    cpdef void add_rule(self,object type_,LexicRule rule)
--- lexer/lexer.pyx
+++ lexer/lexer.pyx
@@ -1,15 +1,39 @@
-from typing import Callable,Any,Tuple
+from typing import Callable,Any,Tuple,Iterable,Set
 
-from common.types cimport Symbol
+from common.types cimport Symbol,Token
 from automaton.automaton cimport DFA,State
 from regex.engine cimport _parse
 from .base_lexer cimport BaseLexer
+from analisis.lexical cimport LexicRule
+from analisis.error cimport LexicError
 
 cdef class Lexer(BaseLexer):
     
     def __init__(self, get_symbol_function: Callable[[Any, str], Symbol], ignore_pattern: DFA) -> None:
         super().__init__(get_symbol_function, ignore_pattern)
+        self._rules = {}
+        self._errors = set()
     
+    @property
+    def errors(self) -> Set[LexicError]:
+        return self._errors.copy()
+
+    @property
+    def tokens(self) -> Iterable[Token]:
+        cdef LexicError error
+        cdef object key
+        cdef LexicRule rule
+        self.initialize()
+        while self._move_next():
+            if self._ignore.accept(list(self._current_token._text)):
+                continue
+            for key in self._rules:
+                for rule in self._rules[key]:
+                    error = rule.check(self._current_token)
+                    if not error is None:
+                        self._errors.add(error)
+            yield self._current_token
+
     def __setitem__(self, key: Tuple[int, object], re:str):
         cdef DFA dfa = _parse(re)
         cdef State state
@@ -19,3 +43,8 @@ cdef class Lexer(BaseLexer):
                 state._value = key[1]
         
         self._add_token(key[0],key[1],dfa)
+        if not key[1] in self._rules:
+            self._rules[key[1]] = set()
+    
+    cpdef void add_rule(self,object type_,LexicRule rule):
+        self._rules[type_].add(rule)
