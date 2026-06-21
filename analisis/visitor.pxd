4af43249c405dba17af11080f7a509dabf5ad197
YonyUk <yonatanjoseguerraperez@gmail.com>
2026-06-21 17:43:16 -0400

    adds TraversalStrategy

--- analisis/visitor.pxd
+++ analisis/visitor.pxd
@@ -10,12 +10,22 @@ cdef class ASTChildrenSelector:
 
     cpdef list[AST] select_children(self,AST ast, Context context)
 
+cdef class TraversalStrategy:
+    cdef AST _root
+    cdef dict[type,ASTChildrenSelector] _selectors
+
+    cpdef void init(self,AST root)
+    cpdef bint has_next(self)
+    cpdef AST current(self,Context context)
+    cpdef void reset(self)
+    cpdef ASTChildrenSelector _get_selector(self,AST ast)
+    cpdef void add_selector(self,type ast_type,ASTChildrenSelector selector)
+
 cdef class ASTWalker:
 
     cdef Context _context
     cdef dict[type,ASTVisitor] _visitors
-    cdef dict[type,ASTChildrenSelector] _selectors
+    cdef TraversalStrategy _strategy
 
     cpdef void walk(self,AST ast)
     cpdef void add_visitor(self,type ast_type,ASTVisitor visitor)
-    cpdef void add_selector(self,type ast_type,ASTChildrenSelector selector)
--- analisis/visitor.pyx
+++ analisis/visitor.pyx
@@ -27,49 +27,55 @@ cdef class ASTChildrenSelector:
         raise NotImplementedError()
 
 
-cdef class ASTWalker:
+cdef class TraversalStrategy:
 
-    def __init__(self,Context context) -> None:
-        self._visitors = {}
+    def __init__(self) -> None:
         self._selectors = {}
-        self._context = context
+        pass
     
+    cpdef void init(self,AST root):
+        self._root = root
+
+    cpdef bint has_next(self):
+        raise NotImplementedError()
+    
+    cpdef AST current(self,Context context):
+        raise NotImplementedError()
+
+    cpdef void reset(self):
+        raise NotImplementedError()
+    
+    cpdef ASTChildrenSelector _get_selector(self,AST ast):
+        cdef type ast_type = type(ast) # type:ignore
+        return self._selectors[ast_type] # type:ignore
+
     cpdef void add_selector(self,type ast_type,ASTChildrenSelector selector):
         self._selectors[ast_type] = selector # type:ignore
+
+cdef class ASTWalker:
+
+    def __init__(self,Context context,TraversalStrategy strategy) -> None:
+        self._visitors = {}
+        self._context = context
+        self._strategy = strategy # type:ignore
 
     cpdef void add_visitor(self,type ast_type,ASTVisitor visitor):
         self._visitors[ast_type] = visitor # type:ignore
     
     cpdef void walk(self,AST ast):
-        cdef list[AST] stack = []
         cdef AST current
-        cdef list[AST] seen = []
         cdef type ast_type # type:ignore
         cdef ASTVisitor visitor
         cdef ASTChildrenSelector selector
         cdef SemanticError error
-        cdef AST child
-        cdef bint asts_added = False # type:ignore
 
-        ast_type = type(ast)
-        if ast_type in self._selectors:
-            selector = self._selectors[ast_type] # type:ignore
-            stack += selector.select_children(ast,self._context)
+        self._strategy.init(ast)
 
-        while stack:
-            asts_added = False # type:ignore
-            current = stack[-1]
-            seen.append(current)
+        while self._strategy.has_next():
+            current = self._strategy.current(self._context)
             ast_type = type(current)
-            if ast_type in self._selectors:
-                selector = self._selectors[ast_type] # type:ignore
-                for child in selector.select_children(current,self._context):
-                    if not child in stack and not child in seen:
-                        stack.append(child)
-                        asts_added = True # type:ignore
-            if asts_added:
-                continue
             if ast_type in self._visitors:
                 visitor = self._visitors[ast_type] # type:ignore
                 visitor.visit(current,self._context)
-            stack.pop()
+        
+        self._strategy.reset()
