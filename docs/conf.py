a6b1ddfc607f7daf93d044f241a615062c843ca1
YonyUk <yonatanjoseguerraperez@gmail.com>
2026-08-16 21:29:22 -0400

    fix meta tag

--- docs/conf.py
+++ docs/conf.py
@@ -4,7 +4,7 @@
 
 def add_google_verification(app, pagename, templatename, context, doctree):
     # Este es el meta tag que Google necesita para verificar el sitio
-    meta_tag = '<meta name="google-site-verification" content="google237ade8f7069cc42" />'
+    meta_tag = '<meta name="google-site-verification" content="WY0eAxGSQVsrz78bXP9Efi4kzl6UXmljaZ51YnEyHDQ" />'
     # Lo añadimos a la cabecera (head) de todas las páginas
     context['metatags'] = context.get('metatags', '') + meta_tag
 
