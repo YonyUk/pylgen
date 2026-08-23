87df99ee1135a93473a3a7d119279a4b24222f1b
YonyUk <yonatanjoseguerraperez@gmail.com>
2026-08-22 23:14:44 -0400

    adds tests to visual module to test lr_inspection

---
+++ tests/integration/visual/test_lr_inspection.py
@@ -0,0 +1,170 @@
+from pylgen.visual.table import build_action_goto_slr_tables,build_action_goto_lalr_tables
+from pylgen.common.types import Symbol
+from pylgen.grammar import Grammar
+
+import pytest
+
+class TestVisualLrInspection:
+
+    @pytest.fixture
+    def classic_lalr_grammar(self) -> Grammar:
+        S = Symbol('S')
+        L = Symbol('L')
+        R = Symbol('R')
+
+        mul = Symbol('*',True)
+        id_ = Symbol('id',True)
+        eq = Symbol('=',True)
+
+        G = Grammar(S,'$')
+
+        G[S] += L,eq,R
+        G[S] += R,
+
+        G[L] += mul,R
+        G[L] += id_,
+
+        G[R] += L,
+
+        return G
+
+    @pytest.fixture
+    def classic_lalr_grammar_with_reduce_reduce_conflict(self) -> Grammar:
+        S = Symbol('S')
+        L = Symbol('L')
+        R = Symbol('R')
+
+        mul = Symbol('*',True)
+        id_ = Symbol('id',True)
+        eq = Symbol('=',True)
+
+        G = Grammar(S,'$')
+
+        G[S] += L,eq,R
+        G[S] += R,
+
+        G[L] += mul,R
+        G[L] += id_,
+
+        G[R] += L,
+        G[R] += id_,
+
+        return G
+
+    @pytest.fixture
+    def classic_arithmetic_grammar(self) -> Grammar:
+        E = Symbol('E')
+        T = Symbol('T')
+
+        plus = Symbol('+',True)
+        n = Symbol('n',True)
+
+        G = Grammar(E,'$')
+
+        G[E] += E,plus,T
+        G[E] += T,
+
+        G[T] += n,
+
+        return G
+
+    @pytest.fixture
+    def classic_arithmetic_grammar_with_reduce_reduce_conflict(self) -> Grammar:
+        E = Symbol('E')
+        T = Symbol('T')
+
+        plus = Symbol('+',True)
+        n = Symbol('n',True)
+
+        G = Grammar(E,'$')
+
+        G[E] += E,plus,T
+        G[E] += T,
+        G[E] += n,
+
+        G[T] += n,
+
+        return G
+
+    def test_lalr_tables_1(self,classic_lalr_grammar:Grammar):
+
+        action,_ = build_action_goto_lalr_tables(classic_lalr_grammar)
+
+        for (_,symbol),actions in action.items():
+            if not symbol.is_terminal:
+                continue
+            if actions:
+                assert len(actions) == 1
+
+    def test_slr_tables_1(self,classic_arithmetic_grammar:Grammar):
+
+        action,_ = build_action_goto_slr_tables(classic_arithmetic_grammar)
+
+        for (_,symbol),actions in action.items():
+            if not symbol.is_terminal:
+                continue
+            if actions:
+                assert len(actions) == 1
+
+    def test_lalr_tables_2(self,classic_lalr_grammar_with_reduce_reduce_conflict:Grammar):
+        action,_ = build_action_goto_lalr_tables(classic_lalr_grammar_with_reduce_reduce_conflict)
+
+        for (state,symbol),actions in action.items():
+            if not symbol.is_terminal:
+                continue
+            if actions:
+                if state.index == 3 and (symbol.symbol == '=' or symbol.symbol == '$'):
+                    assert len(actions) == 2
+                    assert actions[0][0] == actions[1][0]
+                else:
+                    assert len(actions) == 1
+
+    def test_slr_tables_2(self,classic_lalr_grammar:Grammar):
+
+        action,_ = build_action_goto_slr_tables(classic_lalr_grammar)
+
+        for (state,symbol),actions in action.items():
+            if not symbol.is_terminal:
+                continue
+            if actions:
+                if state.index == 5 and symbol.symbol == '=':
+                    assert len(actions) == 2
+                    assert actions[0][0] != actions[1][0]
+                else:
+                    assert len(actions) == 1
+
+    def test_lalr_tables_3(self):
+        E = Symbol('E')
+        n = Symbol('n',True)
+        plus = Symbol('+',True)
+
+        G = Grammar(E,'$')
+
+        G[E] += E,plus,E
+        G[E] += n,
+
+        action,_ = build_action_goto_lalr_tables(G)
+
+        for (state,symbol),actions in action.items():
+            if not symbol.is_terminal:
+                continue
+            if actions:
+                if state.index == 4 and symbol.symbol == '+':
+                    assert len(actions) == 2
+                    assert actions[0][0] != actions[1][0]
+                else:
+                    assert len(actions) == 1
+
+    def test_slr_tables_3(self,classic_arithmetic_grammar_with_reduce_reduce_conflict:Grammar):
+
+        action,_ = build_action_goto_slr_tables(classic_arithmetic_grammar_with_reduce_reduce_conflict)
+
+        for (state,symbol),actions in action.items():
+            if not symbol.is_terminal:
+                continue
+            if actions:
+                if state.index == 2 and (symbol.symbol == '+' or symbol.symbol == '$'):
+                    assert len(actions) == 2
+                    assert actions[0][0] == actions[1][0]
+                else:
+                    assert len(actions) == 1
