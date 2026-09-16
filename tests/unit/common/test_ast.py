f755f9df354a89b6fc1f1f08d855ca8243dbeeb3
YonyUk <yonatanjoseguerraperez@gmail.com>
2026-09-15 22:22:10 -0400

    updates tests for common module after API change

--- tests/unit/common/test_ast.py
+++ tests/unit/common/test_ast.py
@@ -25,21 +25,40 @@ class TestAST:
     ])
     def test_correct_ast_creation(self,symbol:Symbol,line:int,column:int):
 
-        ast = AST(symbol,line,column)
+        ast = AST(symbol,line,column,line,column+1)
 
         assert ast.symbol == symbol
-        assert ast.line == line
-        assert ast.column == column
+        assert ast.start_position == (line,column)
+        assert ast.end_position == (line,column+1)
         assert not ast.is_error
         with pytest.raises(NotImplementedError):
             ast.children()
 
-        error = SemanticError('error 1',line,column,column,column + 1)
-        error_ast = ErrorAST(symbol,line,column,{error})
+        error = SemanticError('error 1',line,column,line,column + 1)
+        error_ast = ErrorAST(symbol,line,column,line,column+1,{error})
 
         assert error_ast.symbol == symbol
-        assert error_ast.line == line
-        assert error_ast.column == column
+        assert error_ast.start_position == (line,column)
+        assert error_ast.end_position == (line,column+1)
         assert error_ast.is_error
         with pytest.raises(NotImplementedError):
             ast.children()
+
+    def test_invalid_ast_creation(self):
+        symbol = Symbol('s')
+
+        with pytest.raises(ValueError,match='start_line and start_column must be non-negative values'):
+            ast = AST(symbol,-1,1,1,1)
+
+        with pytest.raises(ValueError,match='start_line and start_column must be non-negative values'):
+            ast = AST(symbol,1,-1,1,1)
+
+        with pytest.raises(ValueError,match='end_line cannot be less than start_line'):
+            ast = AST(symbol,1,1,0,1)
+
+        ast = AST(symbol,1,1,1,2)
+
+        with pytest.raises(ValueError,match='start_column must be less than end_column'):
+            ast = AST(symbol,1,1,1,0)
+
+        ast = AST(symbol,1,1,2,0)
--- tests/unit/common/test_error.py
+++ tests/unit/common/test_error.py
@@ -11,12 +11,11 @@ class TestError:
         (ErrorType.SEMANTIC,2,13,'aqui'),
     ])
     def test_error_creation(self,error_type:ErrorType,line:int,column:int,msg:str):
-        error = Error(error_type,line,column,column,column + 1,msg)
+        error = Error(error_type,line,column,line,column + 1,msg)
 
         assert error.type == error_type
-        assert error.line == line
-        assert error.column == column
-        assert error.source_line_interval == (column,column + 1)
+        assert error.start_position == (line,column)
+        assert error.end_position == (line,column+1)
         assert msg in error.message
     
     def test_error_creation_failed(self):
@@ -26,57 +25,54 @@ class TestError:
             error = Error('nada',0,0,0,1,'') # type:ignore
     
     def test_lexic_error(self):
-        error = LexicalError('nada',1,1,0,1)
-        assert error.line == 1
-        assert error.column == 1
+        error = LexicalError('nada',1,1,1,2)
+        assert error.start_position == (1,1)
+        assert error.end_position == (1,2)
         assert error.type == ErrorType.LEXICAL
-        assert error.source_line_interval == (0,1)
         assert 'nada' in error.message
 
     def test_sintax_error(self):
-        error = SyntaxError('nada',1,1,0,1)
-        assert error.line == 1
-        assert error.column == 1
+        error = SyntaxError('nada',1,1,1,2)
+        assert error.start_position == (1,1)
+        assert error.end_position == (1,2)
         assert error.type == ErrorType.SYNTAX
-        assert error.source_line_interval == (0,1)
         assert 'nada' in error.message
 
     def test_semantic_error(self):
-        error = SemanticError('nada',1,1,0,1)
-        assert error.line == 1
-        assert error.column == 1
+        error = SemanticError('nada',1,1,1,2)
+        assert error.start_position == (1,1)
+        assert error.end_position == (1,2)
         assert error.type == ErrorType.SEMANTIC
-        assert error.source_line_interval == (0,1)
         assert 'nada' in error.message
 
     def test_errors_comparision(self):
-        err1 = LexicalError('nada',1,1,0,1)
-        err2 = LexicalError('nada',1,1,0,1)
+        err1 = LexicalError('nada',1,1,1,2)
+        err2 = LexicalError('nada',1,1,1,2)
 
         assert err1 == err2
         assert hash(err1) == hash(err2)
 
-        err2 = LexicalError('nada1',1,1,0,1)
+        err2 = LexicalError('nada1',1,1,1,2)
 
         assert err1 != err2
         assert hash(err1) != hash(err2)
 
-        err2 = LexicalError('nada',1,1,0,2)
+        err2 = LexicalError('nada',1,1,1,3)
 
         assert err1 != err2
         assert hash(err1) != hash(err2)
 
-        err2 = SyntaxError('nada',1,1,0,1)
+        err2 = SyntaxError('nada',1,1,1,2)
 
         assert err1 != err2
         assert hash(err1) != hash(err2)
 
-        err2 = SemanticError('nada',1,1,0,1)
+        err2 = SemanticError('nada',1,1,1,2)
 
         assert err1 != err2
         assert hash(err1) != hash(err2)
 
-        err2 = RuntimeError([],1,1,0,1,'nada')
+        err2 = RuntimeError([],1,1,1,2,'nada')
 
         assert err1 != err2
         assert hash(err1) != hash(err2)
